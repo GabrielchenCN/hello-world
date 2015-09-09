0baf79ae9f46ac5afc58f5d2ac1dbcc0a1d03aac
siming,chen <317755940@qq.com>
2015-09-09 22:11:42 +0800

    Update randomNumber.js

--- html/randomNumber.js
+++ html/randomNumber.js
@@ -12,4 +12,18 @@ document.write(originalArray[i]+" , ");
 } 
 var d2=new Date().getTime(); 
 document.write("运算耗时"+(d2-d1)); 
+var randomNumber = function(count){
+  var originalArray=new Array();//原数组 
+//给原数组originalArray赋值 
+for (var i=0;i<count;i++){ 
+originalArray[i]=i+1; 
+} 
+var d1=new Date().getTime(); 
+originalArray.sort(function(){ return 0.5 - Math.random(); }); 
+for (var i=0;i<count;i++){ 
+
+var d2=new Date().getTime(); 
+var time = d2-d1;
+return originalArray ;
+}
 </script> 
